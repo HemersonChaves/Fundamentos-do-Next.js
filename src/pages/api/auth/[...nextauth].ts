3643f274cd69069c48006810e52acbb40bcf3fb3
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-08-30 16:44:12 -0300

    feat(nextauth): verificando usuário duplicado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
-import {query as consulta_bd} from "faunadb";
+import { query as consulta_bd } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import {fauna} from "../../../services/fauna";
+import { fauna } from "../../../services/fauna";
 
 
 export default NextAuth({
@@ -12,18 +12,41 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.SIGNING_KEY,
   callbacks: {
     async signIn({ user, account, profile }) {
       const { email } = user;
-      
-      await fauna.query(
-        consulta_bd.Create(
-          consulta_bd.Collection("users"),
-          { data:{email}}
+      try {
+        await fauna.query(
+          consulta_bd.If(
+            consulta_bd.Not(
+              consulta_bd.Exists(
+                consulta_bd.Match(
+                  consulta_bd.Index("user_by_email"),
+                  consulta_bd.Casefold(user.email)
+                )
+              )
+            ),
+            consulta_bd.Create(
+              consulta_bd.Collection("users"),
+              { data: { email } }
+            ),
+            consulta_bd.Get(
+
+              consulta_bd.Match(
+                consulta_bd.Index("user_by_email"),
+                consulta_bd.Casefold(user.email)
+              )
+            )
+          )
+
         )
-      )
-      // console.log(user);
-      return true
+        // console.log(user);
+        return true
+      } catch (err) {
+        console.log(err)
+        return false;
+      }
     },
   }
 })
