b7668006a0658bb0c588b2abc86025821e86b4df
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-08-31 19:29:34 -0300

    refactor(SubscribeButton): cria uma function handleSubscribe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { useSession, signIn } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe_js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -5,10 +8,28 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
+    const { data: session } = useSession();
+   async function handleSubscribe(){
+        if (!session){
+            signIn("github")
+            return;
+        }
+        // criação da checkout session
+        try {
+            const response = await api.post("/subscribe");
+            const{ sessionId } = response.data;
+            const stripe = await getStripeJs();
+            await stripe.redirectToCheckout({sessionId});
+        } catch (error) {
+            alert(error.message);
+        }
+    }
     return (
         <button
             type="button"
+            onClick={() => handleSubscribe()}
             className={styles.subscribeButton}>
+                
             subscribe now
         </button>
     )
