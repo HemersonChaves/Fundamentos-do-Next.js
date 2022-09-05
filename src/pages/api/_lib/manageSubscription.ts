3c805526217bb8b00b2a663086634a4a8a201d5b
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-09-05 16:42:06 -0300

    refactor(webhook):valida checkoutSession

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,9 @@
+export async function saveSubscriptions(
+    subscriptionId: string,
+    customerId: string
+    ){
+        // buscar usuario no banco do FaunaDB com ID {customerId} Stripe.c  ustomerId
+        // Salvar os dados da subscription no FaunaDB
+        console.log(subscriptionId, customerId);
+
+    }
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -3,6 +3,7 @@ import { Readable } from "stream";
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
 import Id from "./users/[id]";
+import { saveSubscriptions } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
     const chunks = [];
@@ -31,15 +32,31 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const secret = req.headers["stripe-signature"]
 
         let event: Stripe.Event;
-        try{ 
+        try {
             event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
-        } catch(err){
+        } catch (err) {
             return res.status(400).send(`Webhook erro: ${err.message}`);
-        }         
-        
-        const {type} = event;
-        if(relevantEvents.has(type)){
-            console.log("evento recebido", event);
+        }
+
+        const { type } = event;
+        if (relevantEvents.has(type)) {
+            try {
+                switch (type) {
+                    case "checkout.session.completed":
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+                        await saveSubscriptions(
+                            checkoutSession.subscription.toString(),
+                            checkoutSession.customer.toString()
+                        )
+                        break;
+                    default:
+                        throw new Error("Unhandled event.")
+                }
+            } catch (err) {
+                // sentry, bugsnag
+                return res.json({ error: "Webhook handler failed." })
+            }
         }
         res.json({ received: true })
     } else {
