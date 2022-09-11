56a33a448b5a5e1c16044b6b78b60f310ad46d5b
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-09-11 15:51:22 -0300

    refactor(subscriton): ouve os eventos do strip para natualizar subscritpions

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { Readable } from "stream";
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
-import { saveSubscriptions } from "./_lib/manageSubscription";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
     const chunks = [];
@@ -21,7 +21,10 @@ export const config = {
     }
 }
 const relevantEvents = new Set([
-    "checkout.session.completed"
+    "checkout.session.completed",
+    "customer.subscription.created",
+    "customer.subscription.updated",
+    "customer.subscription.deleted",
 ])
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -41,25 +44,37 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         if (relevantEvents.has(type)) {
             try {
                 switch (type) {
-                    case "checkout.session.completed":
-                        const checkoutSession = event.data.object as Stripe.Checkout.Session;
+                    case "customer.subscription.created":
+                    case "customer.subscription.updated":
+                    case "customer.subscription.deleted":
+                        const subscription = event.data.object as  Stripe.Subscription;
 
-                        await saveSubscriptions(
-                            checkoutSession.subscription.toString(),
-                            checkoutSession.customer.toString()
-                        )
+                        await saveSubscription(
+                            subscription.id,
+                            subscription.customer.toString(),
+                            type === "customer.subscription.created"
+                        );
                         break;
+                    case "checkout.session.completed":
+                const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+                await saveSubscription(
+                    checkoutSession.subscription.toString(),
+                    checkoutSession.customer.toString(),
+                    true
+                )
+                break;
                     default:
-                        throw new Error("Unhandled event.")
-                }
-            } catch (err) {
-                // sentry, bugsnag
-                return res.json({ error: "Webhook handler failed." })
+                throw new Error("Unhandled event.")
             }
+            } catch (err) {
+            // sentry, bugsnag
+            return res.json({ error: "Webhook handler failed." })
         }
-        res.json({ received: true })
-    } else {
-        res.setHeader("Allow", "POST");
-        res.status(405).end("Método não permitido")
     }
+    res.json({ received: true })
+} else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Método não permitido")
+}
 }
