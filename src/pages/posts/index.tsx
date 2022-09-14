31252339cd8e391d512ab754c9a1fa30cdd20e24
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-09-13 21:24:53 -0300

    refactor(post): retira console.log

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -47,7 +47,7 @@ export const getStaticProps: GetStaticProps = async () => {
         }
     );
 
-    console.log(JSON.stringify(response, null, 2));
+    //console.log(JSON.stringify(response, null, 2));
     const posts = response.results.map(post => {
         return {
             slug: post.uid,
