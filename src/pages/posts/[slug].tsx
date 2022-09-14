6b22783c59ed20ac1a920d69ec4bc86469049dcd
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-09-14 09:27:32 -0300

    feat(post): pagina para consumo de uma postagem

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,9 @@
+import { GetServerSideProps } from "next"
+
+export default function Post(){
+    return (
+        <h1>Teste</h1>
+    )
+}
+
+//export const getServerSideProps: GetServerSideProps = async ({req})
