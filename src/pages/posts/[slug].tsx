2b9e44776889eb2955f99858c64fbe6d6e23653d
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-09-15 11:04:30 -0300

    refactor(nextauth): verifica Subscription ativa e redireciona para post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -35,10 +35,14 @@ export default function Post({ post }: PostProps) {
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
     const session = await getSession({ req });
     const { slug } = params;
-
-    // if(!session){
-
-    // }
+    if(!session.activeSubscription){
+        return{
+            redirect:{
+                destination: "/",
+                permanent: false,
+            }
+        }
+    }
 
     const prismic = getPrismicCleint(req);
 
